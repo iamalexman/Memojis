129abda859d4a997d785cade7fcce09110cca158
iamaleksman <kuznetcov.ios@gmail.com>
2024-05-23 01:52:48 +0300

    workflow and script

---
+++ build-docc.sh
@@ -0,0 +1,13 @@
+##!/bin/sh
+
+xcrun xcodebuild docbuild \
+	-scheme Memojis \
+	-destination 'generic/platform=iOS Simulator' \
+	-derivedDataPath "$PWD/.derivedData"
+	
+xcrun docc process-archive transform-for-static-hosting \
+	"$PWD/.derivedData/Build/Products/Debug-iphonesimulator/Memojis.doccarchive" \
+	--output-path ".docs" \
+	--hosting-base-path "memojis" # add with your repo name later
+	
+echo '<script>window.location.href += "/documentation/memojis"</script>' > .docs/index.html
